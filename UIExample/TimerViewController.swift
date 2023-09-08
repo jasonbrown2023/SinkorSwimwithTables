db8888ef4f1b6f9feda0d18fe85d63e463d95424
Jason Brown <jasonba@smu.edu>
2023-09-07 20:46:44 -0500

    Add timerviewcontroller

---
+++ UIExample/TimerViewController.swift
@@ -0,0 +1,82 @@
+//
+//  TimerViewController.swift
+//  UIExample
+//
+//  Created by jason brown on 13/06/1402 AP.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+    var seconds = 10
+    var timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //timerAction()
+        timer.invalidate()
+    
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerViewController.counter), userInfo: nil, repeats: true)
+        
+        //sliderOutlet.isHidden = true
+        view.backgroundColor = .green
+        startOutlet.isHidden = true
+        
+    }
+    
+    @IBOutlet weak var startOutlet: UIButton!
+    
+    @IBOutlet weak var stopOutlet: UIButton!
+    
+
+    @IBAction func stopButton(_ sender: UIButton) {
+        timer.invalidate()
+        view.backgroundColor = .red
+        seconds = 10
+        //sliderOutlet.setValue(30, animated: true)
+        label.text = "10 seconds"
+        
+        //sliderOutlet.isHidden = false
+        startOutlet.isHidden = false
+    }
+    
+    @objc func counter()
+    {
+        seconds -= 1
+        label.text = String(seconds) + "seconds"
+        if (seconds == 0)
+        {
+            view.backgroundColor = .yellow
+            timer.invalidate()
+            label.text = "Times up!"
+            //sliderOutlet.isHidden = false
+            startOutlet.isHidden = false
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerViewController.counter), userInfo: nil, repeats: true)
+            seconds = 10
+            
+            startOutlet.isHidden = true
+        }
+    }
+    
+    
+   
+     
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
